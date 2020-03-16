179cb5a45514293c84c28c3009ed120d599d7b0d
Diego Gómez <dagomez2530@gmail.com>
2020-03-16 23:52:27 +0000

    New function 1-print_integer.c, and added new changes to holberton.h

--- holberton.h
+++ holberton.h
@@ -4,7 +4,7 @@
 
 int print_char(char c);
 int print_string(va_list list);
-int print_integer(int n);
+int print_integer(int n, int count);
 int print_double(long int n);
 typedef struct newdatatype
 {
